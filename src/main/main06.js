eab958ed8f21212481a8aa27daaeb3164eca2bfa
caijianhaoa <1784581164@qq.com>
2022-11-12 17:13:42 +0800

    透明材质和透明纹理

---
+++ src/main/main06.js
@@ -0,0 +1,84 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import gsap from 'gsap'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+camera.position.set(0, 0, 10)
+scene.add(camera)
+
+const textureLoader = new THREE.TextureLoader()
+const doorColorTexture = textureLoader.load('./textures/door/color.jpg')
+const doorAplhaTexture = textureLoader.load('./textures/door/alpha.jpg')
+// doorColorTexture.offset.set(0.5, 0.5)
+// doorColorTexture.center.set(0.5, 0.5)
+// doorColorTexture.rotation = Math.PI / 4
+// doorColorTexture.repeat.set(2, 3)
+// doorColorTexture.wrapS = THREE.RepeatWrapping
+// doorColorTexture.wrapT = THREE.RepeatWrapping
+
+const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
+const basicMaterial = new THREE.MeshBasicMaterial({
+  color: '#ffff00',
+  map: doorColorTexture,
+  alphaMap: doorAplhaTexture,
+  transparent: true,
+  side: THREE.DoubleSide,
+})
+
+const cube = new THREE.Mesh(cubeGeometry, basicMaterial)
+
+scene.add(cube)
+
+const plane = new THREE.Mesh(new THREE.PlaneBufferGeometry(1, 1), basicMaterial)
+
+plane.position.set(3, 0, 0)
+scene.add(plane)
+
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+// renderer.render(scene, camera)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+const axesHelper = new THREE.AxesHelper(5)
+
+scene.add(axesHelper)
+
+const clock = new THREE.Clock()
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement = document.fullscreenElement
+  if (!fullscreenElement) {
+    renderer.domElement.requestFullscreen()
+  } else {
+    document.exitFullscreen()
+  }
+})
+
+function render() {
+  controls.update()
+  renderer.render(scene, camera)
+  requestAnimationFrame(render)
+}
+
+render()
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+})
