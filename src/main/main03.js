c5c4af4dce807acd89bc38024e7325f04030b2ee
caijianhaoa <1784581164@qq.com>
2022-11-12 16:39:37 +0800

    认识纹理

---
+++ src/main/main03.js
@@ -0,0 +1,69 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import gsap from 'gsap'
+
+const scene = new THREE.Scene()
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+camera.position.set(0, 0, 10)
+scene.add(camera)
+
+const textureLoader = new THREE.TextureLoader()
+const doorColorTextture = textureLoader.load('./textures/door/color.jpg')
+
+const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1)
+const basicMaterial = new THREE.MeshBasicMaterial({
+  color: '#ffff00',
+  map: doorColorTextture,
+})
+
+const cube = new THREE.Mesh(cubeGeometry, basicMaterial)
+
+scene.add(cube)
+
+const renderer = new THREE.WebGLRenderer()
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+document.body.appendChild(renderer.domElement)
+
+// renderer.render(scene, camera)
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+const axesHelper = new THREE.AxesHelper(5)
+
+scene.add(axesHelper)
+
+const clock = new THREE.Clock()
+
+window.addEventListener('dblclick', () => {
+  const fullscreenElement = document.fullscreenElement
+  if (!fullscreenElement) {
+    renderer.domElement.requestFullscreen()
+  } else {
+    document.exitFullscreen()
+  }
+})
+
+function render() {
+  controls.update()
+  renderer.render(scene, camera)
+  requestAnimationFrame(render)
+}
+
+render()
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+  renderer.setPixelRatio(window.devicePixelRatio)
+})
