5ac163d65b63dadf52b798382a7b62d2841a6b6c
caijianhaoa <1784581164@qq.com>
2022-11-14 16:38:00 +0800

    学习着色器

--- src/main/main.js
+++ src/main/main.js
@@ -1,191 +1,81 @@
 import * as THREE from 'three'
-// 导入轨道控制器
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// 导入动画库
 import gsap from 'gsap'
-// 导入dat.gui
 import * as dat from 'dat.gui'
-// 导入connon引擎
-import * as CANNON from 'cannon-es'
 
-// 目标：设置cube跟着旋转
-console.log(CANNON)
+// 顶点着色器
+import basicVertexShader from '../shader/basic/vertex.glsl'
+// 片元着色器
+import basicFragmentShader from '../shader/basic/fragment.glsl'
 
-// const gui = new dat.GUI();
-// 1、创建场景
+// 目标：认识shader
+
+//创建gui对象
+const gui = new dat.GUI()
+
+// console.log(THREE);
+// 初始化场景
 const scene = new THREE.Scene()
 
-// 2、创建相机
+// 创建透视相机
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
+  90,
+  window.innerHeight / window.innerHeight,
   0.1,
-  300
+  1000
 )
-
 // 设置相机位置
-camera.position.set(0, 0, 18)
+// object3d具有position，属性是1个3维的向量
+camera.position.set(0, 0, 2)
+// 更新摄像头
+camera.aspect = window.innerWidth / window.innerHeight
+//   更新摄像机的投影矩阵
+camera.updateProjectionMatrix()
 scene.add(camera)
 
-const cubeArr = []
-//设置物体材质
-const cubeWorldMaterial = new CANNON.Material('cube')
-
-function createCube() {
-  // 创建立方体和平面
-  const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
-  const cubeMaterial = new THREE.MeshStandardMaterial()
-  const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
-  cube.castShadow = true
-  scene.add(cube)
-  // 创建物理cube形状
-  const cubeShape = new CANNON.Box(new CANNON.Vec3(0.5, 0.5, 0.5))
-
-  // 创建物理世界的物体
-  const cubeBody = new CANNON.Body({
-    shape: cubeShape,
-    position: new CANNON.Vec3(0, 0, 0),
-    //   小球质量
-    mass: 1,
-    //   物体材质
-    material: cubeWorldMaterial,
-  })
-  cubeBody.applyLocalForce(
-    new CANNON.Vec3(300, 0, 0), //添加的力的大小和方向
-    new CANNON.Vec3(0, 0, 0) //施加的力所在的位置
-  )
-
-  // 将物体添加至物理世界
-  world.addBody(cubeBody)
-  // 添加监听碰撞事件
-  function HitEvent(e) {
-    // 获取碰撞的强度
-    // console.log("hit", e);
-    const impactStrength = e.contact.getImpactVelocityAlongNormal()
-    console.log(impactStrength)
-    if (impactStrength > 2) {
-      //   重新从零开始播放
-      hitSound.currentTime = 0
-      hitSound.volume = impactStrength / 12
-      hitSound.play()
-    }
-  }
-  cubeBody.addEventListener('collide', HitEvent)
-  cubeArr.push({
-    mesh: cube,
-    body: cubeBody,
-  })
-}
-
-window.addEventListener('click', createCube)
-
-const floor = new THREE.Mesh(
-  new THREE.PlaneGeometry(20, 20),
-  new THREE.MeshStandardMaterial()
-)
-
-floor.position.set(0, -5, 0)
-floor.rotation.x = -Math.PI / 2
-floor.receiveShadow = true
-scene.add(floor)
-
-// 创建物理世界
-// const world = new CANNON.World({ gravity: 9.8 });
-const world = new CANNON.World()
-world.gravity.set(0, -9.8, 0)
-
-// 创建击打声音
-const hitSound = new Audio('assets/metalHit.mp3')
-
-// 物理世界创建地面
-const floorShape = new CANNON.Plane()
-const floorBody = new CANNON.Body()
-const floorMaterial = new CANNON.Material('floor')
-floorBody.material = floorMaterial
-// 当质量为0的时候，可以使得物体保持不动
-floorBody.mass = 0
-floorBody.addShape(floorShape)
-// 地面位置
-floorBody.position.set(0, -5, 0)
-// 旋转地面的位置
-floorBody.quaternion.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2)
-world.addBody(floorBody)
-
-// 设置2种材质碰撞的参数
-const defaultContactMaterial = new CANNON.ContactMaterial(
-  cubeWorldMaterial,
-  floorMaterial,
-  {
-    //   摩擦力
-    friction: 0.1,
-    // 弹性
-    restitution: 0.7,
-  }
-)
-
-// 讲材料的关联设置添加的物理世界
-world.addContactMaterial(defaultContactMaterial)
-
-// 设置世界碰撞的默认材料，如果材料没有设置，都用这个
-world.defaultContactMaterial = defaultContactMaterial
-
-//添加环境光和平行光
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.5)
-dirLight.castShadow = true
-scene.add(dirLight)
-
-// 初始化渲染器
-// 渲染器透明
-const renderer = new THREE.WebGLRenderer({ alpha: true })
-// 设置渲染的尺寸大小
-renderer.setSize(window.innerWidth, window.innerHeight)
-// 开启场景中的阴影贴图
-renderer.shadowMap.enabled = true
-
-// console.log(renderer);
-// 将webgl渲染的canvas内容添加到body
-document.body.appendChild(renderer.domElement)
-
-// // 使用渲染器，通过相机将场景渲染进来
-// renderer.render(scene, camera);
-
-// 创建轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement)
-// 设置控制器阻尼，让控制器更有真实效果,必须在动画循环里调用.update()。
-controls.enableDamping = true
-
-// 添加坐标轴辅助器
+// 加入辅助轴，帮助我们查看3维坐标轴
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
-// 设置时钟
-const clock = new THREE.Clock()
 
-function render() {
-  //   let time = clock.getElapsedTime();
-  let deltaTime = clock.getDelta()
-  // 更新物理引擎里世界的物体
-  world.step(1 / 120, deltaTime)
+// 加载纹理
 
-  //   cube.position.copy(cubeBody.position);
-  cubeArr.forEach((item) => {
-    item.mesh.position.copy(item.body.position)
-    // 设置渲染的物体跟随物理的物体旋转
-    item.mesh.quaternion.copy(item.body.quaternion)
-  })
-
-  renderer.render(scene, camera)
-  //   渲染下一帧的时候就会调用render函数
-  requestAnimationFrame(render)
+// 创建纹理加载器对象
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('./texture/da.jpeg')
+const params = {
+  uFrequency: 10,
+  uScale: 0.1,
 }
 
-render()
+// const material = new THREE.MeshBasicMaterial({ color: "#00ff00" });
+// 创建着色器材质
+const shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: basicVertexShader,
+  fragmentShader: basicFragmentShader,
+})
 
-// 监听画面变化，更新渲染画面
+// 创建平面
+const floor = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(1, 1, 64, 64),
+  shaderMaterial
+)
+
+console.log(floor)
+scene.add(floor)
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer({ alpha: true })
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.BasicShadowMap;
+// renderer.shadowMap.type = THREE.VSMShadowMap;
+
+// 设置渲染尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+// 监听屏幕大小改变的变化，设置渲染的尺寸
 window.addEventListener('resize', () => {
-  //   console.log("画面变化了");
-
+  //   console.log("resize");
   // 更新摄像头
   camera.aspect = window.innerWidth / window.innerHeight
   //   更新摄像机的投影矩阵
@@ -193,6 +83,27 @@ window.addEventListener('resize', () => {
 
   //   更新渲染器
   renderer.setSize(window.innerWidth, window.innerHeight)
-  //   设置渲染器的像素比
+  //   设置渲染器的像素比例
   renderer.setPixelRatio(window.devicePixelRatio)
 })
+
+// 将渲染器添加到body
+document.body.appendChild(renderer.domElement)
+
+// 初始化控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+// 设置控制器阻尼
+controls.enableDamping = true
+// 设置自动旋转
+// controls.autoRotate = true;
+
+const clock = new THREE.Clock()
+function animate(t) {
+  const elapsedTime = clock.getElapsedTime()
+  //   console.log(elapsedTime);
+  requestAnimationFrame(animate)
+  // 使用渲染器渲染相机看这个场景的内容渲染出来
+  renderer.render(scene, camera)
+}
+
+animate()
--- src/main/main01.js
+++ src/main/main01.js
@@ -1,124 +1,89 @@
 import * as THREE from 'three'
-// 导入轨道控制器
+
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-// 导入动画库
 import gsap from 'gsap'
-// 导入dat.gui
 import * as dat from 'dat.gui'
-// 导入connon引擎
-import * as CANNON from 'cannon-es'
 
-// 目标：使用cannon引擎
-console.log(CANNON)
+// 顶点着色器
+import basicVertexShader from '../shader/basic/vertex.glsl'
+// 片元着色器
+import basicFragmentShader from '../shader/basic/fragment.glsl'
 
-// const gui = new dat.GUI();
-// 1、创建场景
+// 目标：认识shader
+
+//创建gui对象
+const gui = new dat.GUI()
+
+// console.log(THREE);
+// 初始化场景
 const scene = new THREE.Scene()
 
-// 2、创建相机
+// 创建透视相机
 const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
+  90,
+  window.innerHeight / window.innerHeight,
   0.1,
-  300
+  1000
 )
-
 // 设置相机位置
-camera.position.set(0, 0, 18)
+// object3d具有position，属性是1个3维的向量
+camera.position.set(0, 0, 2)
+// 更新摄像头
+camera.aspect = window.innerWidth / window.innerHeight
+//   更新摄像机的投影矩阵
+camera.updateProjectionMatrix()
 scene.add(camera)
 
-// 创建球和平面
-const sphereGeometry = new THREE.SphereGeometry(1, 20, 20)
-const sphereMaterial = new THREE.MeshStandardMaterial()
-const sphere = new THREE.Mesh(sphereGeometry, sphereMaterial)
-sphere.castShadow = true
-scene.add(sphere)
-
-const floor = new THREE.Mesh(
-  new THREE.PlaneBufferGeometry(20, 20),
-  new THREE.MeshStandardMaterial()
-)
-
-floor.position.set(0, -5, 0)
-floor.rotation.x = -Math.PI / 2
-floor.receiveShadow = true
-scene.add(floor)
-
-// 创建物理世界
-// const world = new CANNON.World({ gravity: 9.8 });
-const world = new CANNON.World()
-world.gravity.set(0, -9.8, 0)
-// 创建物理小球形状
-const sphereShape = new CANNON.Sphere(1)
-
-//设置物体材质
-const sphereWorldMaterial = new CANNON.Material()
-
-// 创建物理世界的物体
-const sphereBody = new CANNON.Body({
-  shape: sphereShape,
-  position: new CANNON.Vec3(0, 0, 0),
-  //   小球质量
-  mass: 1,
-  //   物体材质
-  material: sphereWorldMaterial,
-})
-
-// 将物体添加至物理世界
-world.addBody(sphereBody)
-
-//添加环境光和平行光
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.5)
-scene.add(ambientLight)
-const dirLight = new THREE.DirectionalLight(0xffffff, 0.5)
-dirLight.castShadow = true
-scene.add(dirLight)
-
-// 初始化渲染器
-// 渲染器透明
-const renderer = new THREE.WebGLRenderer({ alpha: true })
-// 设置渲染的尺寸大小
-renderer.setSize(window.innerWidth, window.innerHeight)
-// 开启场景中的阴影贴图
-renderer.shadowMap.enabled = true
-
-// console.log(renderer);
-// 将webgl渲染的canvas内容添加到body
-document.body.appendChild(renderer.domElement)
-
-// // 使用渲染器，通过相机将场景渲染进来
-// renderer.render(scene, camera);
-
-// 创建轨道控制器
-const controls = new OrbitControls(camera, renderer.domElement)
-// 设置控制器阻尼，让控制器更有真实效果,必须在动画循环里调用.update()。
-controls.enableDamping = true
-
-// 添加坐标轴辅助器
+// 加入辅助轴，帮助我们查看3维坐标轴
 const axesHelper = new THREE.AxesHelper(5)
 scene.add(axesHelper)
-// 设置时钟
-const clock = new THREE.Clock()
 
-function render() {
-  //   let time = clock.getElapsedTime();
-  let deltaTime = clock.getDelta()
-  // 更新物理引擎里世界的物体
-  world.step(1 / 120, deltaTime)
+// 加载纹理
 
-  sphere.position.copy(sphereBody.position)
-
-  renderer.render(scene, camera)
-  //   渲染下一帧的时候就会调用render函数
-  requestAnimationFrame(render)
+// 创建纹理加载器对象
+const textureLoader = new THREE.TextureLoader()
+const texture = textureLoader.load('./texture/da.jpeg')
+const params = {
+  uFrequency: 10,
+  uScale: 0.1,
 }
 
-render()
+// const material = new THREE.MeshBasicMaterial({ color: "#00ff00" });
+// 创建着色器材质
+const shaderMaterial = new THREE.ShaderMaterial({
+  vertexShader: `
+        void main(){
+            gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4( position, 1.0 ) ;
+        }
+    `,
+  fragmentShader: `
+        void main(){
+            gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+        }
+  `,
+})
 
-// 监听画面变化，更新渲染画面
+// 创建平面
+const floor = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(1, 1, 64, 64),
+  shaderMaterial
+)
+
+console.log(floor)
+scene.add(floor)
+
+// 初始化渲染器
+const renderer = new THREE.WebGLRenderer({ alpha: true })
+// renderer.shadowMap.enabled = true;
+// renderer.shadowMap.type = THREE.BasicShadowMap;
+// renderer.shadowMap.type = THREE.VSMShadowMap;
+
+// 设置渲染尺寸大小
+renderer.setSize(window.innerWidth, window.innerHeight)
+
+// 监听屏幕大小改变的变化，设置渲染的尺寸
 window.addEventListener('resize', () => {
-  //   console.log("画面变化了");
-
+  //   console.log("resize");
   // 更新摄像头
   camera.aspect = window.innerWidth / window.innerHeight
   //   更新摄像机的投影矩阵
@@ -126,6 +91,27 @@ window.addEventListener('resize', () => {
 
   //   更新渲染器
   renderer.setSize(window.innerWidth, window.innerHeight)
-  //   设置渲染器的像素比
+  //   设置渲染器的像素比例
   renderer.setPixelRatio(window.devicePixelRatio)
 })
+
+// 将渲染器添加到body
+document.body.appendChild(renderer.domElement)
+
+// 初始化控制器
+const controls = new OrbitControls(camera, renderer.domElement)
+// 设置控制器阻尼
+controls.enableDamping = true
+// 设置自动旋转
+// controls.autoRotate = true;
+
+const clock = new THREE.Clock()
+function animate(t) {
+  const elapsedTime = clock.getElapsedTime()
+  //   console.log(elapsedTime);
+  requestAnimationFrame(animate)
+  // 使用渲染器渲染相机看这个场景的内容渲染出来
+  renderer.render(scene, camera)
+}
+
+animate()
